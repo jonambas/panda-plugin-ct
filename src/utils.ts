813c1e2a86ac167c3ba8ac7cb16e1ca75b4253e6
jonambas <jon@jonambas.com>
2024-04-06 13:01:01 -0400

    refactor: reuse isObject in isObjectWithValue

--- src/utils.ts
+++ src/utils.ts
@@ -1,12 +1,7 @@
-export const isObject = (value: any) => {
-  return typeof value === 'object' && value != null && !Array.isArray(value);
+export const isObject = (obj: any) => {
+  return typeof obj === 'object' && obj != null && !Array.isArray(obj);
 };
 
 export const isObjectWithValue = (obj: any) => {
-  return (
-    typeof obj === 'object' &&
-    obj != null &&
-    !Array.isArray(obj) &&
-    'value' in obj
-  );
+  return isObject(obj) && 'value' in obj;
 };
