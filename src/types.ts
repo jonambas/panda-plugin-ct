27a9569a3ae671dfc9b29d4c69263280095475e2
jonambas <jon@jonambas.com>
2024-04-07 00:41:27 -0400

    chore: remove comments

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,3 @@
-// import { LoggerInterface } from '@pandacss/types';
 import { type Project } from 'ts-morph';
 
 export type ComponentTokens = { [k: string]: string | ComponentTokens };
@@ -7,5 +6,4 @@ export type PluginContext = {
   project: Project;
   tokens: ComponentTokens;
   map: Map<string, string | object>;
-  // logger?: LoggerInterface;
 };
