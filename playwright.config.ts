fdc66a46b64374419c2611059b6484a04b905c8b
jonambas <jon@jonambas.com>
2024-04-11 08:48:03 -0400

    test: rename test dir

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,7 +4,7 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './playwright',
+  testDir: './e2e',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
